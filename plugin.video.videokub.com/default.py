591f478acc0b34619927c3e57635f7615d572a90
Silhouette2022 <Silhouette2022@gmail.com>
2016-12-16 11:44:56 -0600

    videokub - another community fix

--- plugin.video.videokub.com/default.py
+++ plugin.video.videokub.com/default.py
@@ -20,11 +20,12 @@
 # */
 #
 # Writer (c) 2014, MrStealth
-# Rev. 2.0.3.1
+# Rev. 2.0.3.2
 
 import os, urllib, urllib2, sys #, socket, cookielib, errno
 import xbmc, xbmcplugin,xbmcgui,xbmcaddon
 import re
+import json
 
 import XbmcHelpers
 common = XbmcHelpers
@@ -50,7 +51,7 @@ class VideoKub():
         self.language = self.addon.getLocalizedString
         self.inext = os.path.join(self.path, 'resources/icons/next.png')
         self.handle = int(sys.argv[1])
-        self.url = 'http://www.videokub.me/'
+        self.url = 'http://www.videokub.online/'
 
     def main(self):
         params = common.getParameters(sys.argv[2])
@@ -71,8 +72,12 @@ class VideoKub():
             self.index(url, page)
         if mode == 'genres':
             self.genres()
+        if mode == 'history':
+            self.history()
+        if mode == 'clean':
+            self.clean()
         if mode == 'search':
-            self.search(keyword, unified)
+            self.search(keyword, unified, page)
         elif mode == None:
             self.menu()
 
@@ -81,16 +86,39 @@ class VideoKub():
         item = xbmcgui.ListItem("[COLOR=FF00FF00][%s][/COLOR]" % self.language(1000), iconImage=self.icon, thumbnailImage=self.icon)
         xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
 
+        uri = sys.argv[0] + '?mode=%s' % ("history")
+        item = xbmcgui.ListItem("[COLOR=FF00FF00][История поиска][/COLOR]" , iconImage=self.icon, thumbnailImage=self.icon)
+        xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
+		
         uri = sys.argv[0] + '?mode=%s&url=%s' % ("genres", self.url)
         item = xbmcgui.ListItem("[COLOR=FF00FFF0]%s[/COLOR]" % self.language(1003), iconImage=self.icon, thumbnailImage=self.icon)
         xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
 
-        self.index('http://www.videokub.me/latest-updates/', 1)
+        self.index('http://www.videokub.online/latest-updates/', 1)
         xbmcplugin.endOfDirectory(self.handle, True)
 
+    def history(self):    
+        uri = sys.argv[0] + '?mode=%s' % ("clean")
+        item = xbmcgui.ListItem("[COLOR=FF00FF00][Очистить][/COLOR]", iconImage=self.icon, thumbnailImage=self.icon)
+        xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
+        
+        words = []
+        history = self.addon.getSetting('history')
+        if history:
+        	words = history.split(",")
+        
+        for word in reversed(words):
+        	uri = sys.argv[0] + '?mode=%s&keyword=%s' % ("search", word)
+        	item = xbmcgui.ListItem(word, iconImage=self.icon, thumbnailImage=self.icon)
+        	xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
+        xbmcplugin.endOfDirectory(self.handle, True)
+
+    def clean(self):
+		self.addon.setSetting('history', '')
+      
     def genres(self):
 
-        url = 'http://www.videokub.me/categories/'
+        url = 'http://www.videokub.online/categories/'
         response = common.fetchPage({"link": url})
         block_content = common.parseDOM(response["content"], "div", attrs={"class": "block_content"})
 
@@ -142,32 +170,119 @@ class VideoKub():
         xbmcplugin.endOfDirectory(self.handle, True)
 
     def show(self, url):
-        print "Get video %s" % url
-        response = common.fetchPage({"link": url})
-        content = response["content"]
-        scripts = common.parseDOM(response["content"], "script", attrs={"class": "splayer"})
-        title = common.parseDOM(response["content"], "div", attrs={"class": "title"})[0]
-        urls = []
+		print "Get video %s" % url
+		response = common.fetchPage({"link": url})
+		title = common.parseDOM(response["content"], "div", attrs={"class": "title"})[0]
+		content = response["content"]
+		if 'youtube.com/embed' in content:
+			videoId = re.findall('youtube.com/embed/(.*?)[\"\']', content)[0]
+			link = urllib.quote_plus('plugin://plugin.video.youtube/play/?video_id=' + videoId)
+		elif '1tv.ru/embed/' in content:
+			videoId = re.findall('1tv.ru/embed/(.*?)[\"\']', content)[0]
+			player = videoId.split(':')[-1]
+			if '11' in player:
+				url = 'https://www.1tv.ru/video_materials.json?news_id='+videoId.split(':')[0]
+			if '12' in player:
+				url = 'https://www.1tv.ru/video_materials.json?video_id='+videoId.split(':')[0]
+			if '15' in player:
+				url = 'https://www.1tv.ru/video_materials.json?legacy_id='+videoId.split(':')[0]
+			if '16' in player:
+				url = 'https://www.1tv.ru/video_materials.json?collection_id='+videoId.split(':')[0]
+			if '17' in player:
+				url = 'https://www.1tv.ru/video_materials.json?sort=none&type=news&legacy_id='+videoId.split(':')[0]
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			jsonDict = json.loads(resp)
+			link = urllib.quote_plus('http:'+jsonDict[0]['mbr'][0]['src'])
+		elif 'player.stb.ua/embed' in content or 'player.ictv.ua/embed' in content:
+			if 'player.ictv.ua/embed' in content:
+				url = re.findall('(player.ictv.ua/embed/.*?)[\"\']', content)[0]
+				url = 'http://'+url
+			if 'player.stb.ua/embed' in content:
+				url = re.findall('(player.stb.ua/embed/.*?)[\"\']', content)[0]
+				url = 'http://'+url
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			link = urllib.quote_plus(common.parseDOM(resp, "source", attrs={"label": "mq"}, ret="src")[0])
+		elif 'ovva.tv/video/embed' in content:
+			url = re.findall('(ovva.tv/video/embed/.*?)[\"\']', content)[0]
+			url = 'https://'+url
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			resp = re.findall("ovva\('(.*?)'", resp)[0]
+			import base64
+			resp = base64.b64decode(resp)
+			jsonDict = json.loads(resp)
+			link = jsonDict['url']
+			response = urllib2.urlopen(link)
+			resp = response.read()
+			link = urllib.quote_plus(resp.split('=')[-1])
+		elif 'player.vgtrk.com/iframe' in content:
+			videoId = re.findall('player.vgtrk.com/iframe/video/id/(.*?)/', content)[0]
+			url = 'http://player.vgtrk.com/iframe/datavideo/id/'+videoId+'/sid/russiatv'
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			jsonDict = json.loads(resp)
+			link = urllib.quote_plus(jsonDict['data']['playlist']['medialist'][0]['sources']['m3u8']['auto'])
+		elif 'tvc.ru/video/iframe' in content:
+			videoId = re.findall('tvc.ru/video/iframe/id/(.*?)/', content)[0]
+			url = 'http://www.tvc.ru/video/json/id/'+videoId
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			jsonDict = json.loads(resp)
+			link = urllib.quote_plus('http:'+jsonDict['path']['quality'][0]['url'])
+		elif 'rutube.ru/play/embed/' in content:
+			videoId = re.findall('rutube.ru/play/embed/(.*?)[\"\']', content)[0]
+			url = 'http://rutube.ru/api/play/options/'+videoId+'?format=json'
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			jsonDict = json.loads(resp)
+			link = urllib.quote_plus(jsonDict['video_balancer']['m3u8'])
+		elif 'ntv.ru/video/embed/' in content:
+			url = re.findall('(ntv.ru/video/embed/.*?)[\"\']', content)[0]
+			url = 'http://'+url
+			request = urllib2.Request(url)
+			request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+			response = urllib2.urlopen(request)
+			resp = response.read()
+			resp = re.findall("CDATA\[(.*?)\]", resp)
+			for r in resp:
+				if 'mp4' in r and '_lo' not in r and 'mobile' not in r:
+					link = urllib.quote_plus('http://media.ntv.ru/vod'+r)
+					break
+		else:	
+			scripts = common.parseDOM(response["content"], "script", attrs={"class": "splayer"})
+			urls = []
+			for script in scripts:
+				if 'mp4' in script:
+					urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+.mp4', script)
 
-        for script in scripts:
-            if 'mp4' in script:
-                urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+.mp4', script)
+			link = urls[-1]
 
-        link = urls[-1]
+		print "link %s" % link
 
-        print "link %s" % link
+		#search_string = title.split(' ')
 
-        search_string = title.split(' ')
+		# 'http://www.videokub.me/search/?q=%s' % (search_string[0] + ' ' + search_string[1])
 
-        # 'http://www.videokub.me/search/?q=%s' % (search_string[0] + ' ' + search_string[1])
-
-        uri = sys.argv[0] + '?mode=play&url=%s' % link
-        item = xbmcgui.ListItem(title, thumbnailImage=self.icon, iconImage=self.icon)
-        item.setInfo(type='Video', infoLabels={'title': title, 'overlay': xbmcgui.ICON_OVERLAY_WATCHED, 'playCount': 0})
-        item.setProperty('IsPlayable', 'true')
-        xbmcplugin.addDirectoryItem(self.handle, uri, item, False)
-
-        xbmcplugin.endOfDirectory(self.handle, True)
+		uri = sys.argv[0] + '?mode=play&url=%s' % link
+		item = xbmcgui.ListItem(title, thumbnailImage=self.icon, iconImage=self.icon)
+		item.setInfo(type='Video', infoLabels={'title': title, 'overlay': xbmcgui.ICON_OVERLAY_WATCHED, 'playCount': 0})
+		item.setProperty('IsPlayable', 'true')
+		xbmcplugin.addDirectoryItem(self.handle, uri, item, False)
+		xbmcplugin.endOfDirectory(self.handle, True)
 
     def getUserInput(self):
         kbd = xbmc.Keyboard()
@@ -175,26 +290,42 @@ class VideoKub():
         kbd.setHeading(self.language(1000))
         kbd.doModal()
         keyword = None
+        s = None
 
         if kbd.isConfirmed():
+            s = kbd.getText()
             if self.addon.getSetting('translit') == 'true':
-                keyword = translit.rus(kbd.getText())
+                try:
+                	keyword = translit.rus(s)	
+                except:pass
+                try:
+                	keyword = self.encode(keyword)	
+                except:keyword = s      
             else:
-                keyword = kbd.getText()
+                keyword = s
+        words = []
+        history = self.addon.getSetting('history')
+        if history:
+        	words = history.split(",")
+        if keyword and keyword not in words:
+        	words.append(keyword)
+        	self.addon.setSetting('history', ','.join(words))
         return keyword
 
-    def search(self, keyword, unified):
+    def search(self, keyword, unified, page):
         print "*** Search: unified %s" % unified
 
-        keyword = translit.rus(keyword) if unified else self.getUserInput()
-        unified_search_results = []
-
+        if not keyword:
+        	keyword = self.getUserInput()
+        
         if keyword:
-            keyword = self.encode(keyword)
-
-            url = 'http://www.videokub.me/search/?q=%s' % (keyword)
-            response = urllib2.urlopen(url)
-            content = common.parseDOM(response.read(), "div", attrs={"class": "list_videos"})
+            
+            url = 'http://www.videokub.online/search/'+str(page)+'/?q=%s' % (keyword)
+            request = urllib2.Request(url)
+            request.add_header('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
+            response = urllib2.urlopen(request)
+            resp = response.read()
+            content = common.parseDOM(resp, "div", attrs={"class": "list_videos"})
             videos = common.parseDOM(content, "div", attrs={"class": "short"})
 
             links = common.parseDOM(videos, "a", attrs={"class": "kt_imgrc"}, ret='href')
@@ -202,6 +333,10 @@ class VideoKub():
             images = common.parseDOM(videos, "img", attrs={"class": "thumb"}, ret='src')
 
             durations = common.parseDOM(videos, "span", attrs={"class": "time"})
+            
+            pages = common.parseDOM(resp, "div", attrs={"class": "pagination navigation"})
+            pages = common.parseDOM(pages, "div", attrs={"class": "block_content"})
+            pages = common.parseDOM(pages, "a", ret='href')
 
             if unified:
                 print "Perform unified search and return results"
@@ -227,6 +362,11 @@ class VideoKub():
                 #item = xbmcgui.ListItem(self.language(1004), iconImage=self.inext)
                 #xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
 
+                if int(page)<=len(pages):
+                	uri = sys.argv[0] + '?mode=%s&keyword=%s&page=%s' % ("search", keyword, str(int(page)+1))
+                	item = xbmcgui.ListItem("следующая страница...", iconImage=self.icon, thumbnailImage=self.icon)
+                	xbmcplugin.addDirectoryItem(self.handle, uri, item, True)
+
                 xbmc.executebuiltin('Container.SetViewMode(52)')
                 xbmcplugin.endOfDirectory(self.handle, True)
 
@@ -250,6 +390,7 @@ class VideoKub():
         # xbmcplugin.endOfDirectory(self.handle, True)
 
     def play(self, url):
+        print 'url to play '+url
         item = xbmcgui.ListItem(path = url)
         xbmcplugin.setResolvedUrl(self.handle, True, item)
 
